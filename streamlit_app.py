a0609d21c1d5222ff0b0cbc5255b03daea76464c
JuanPabloRosas <juanpablo.rosasbaldazo@gmail.com>
2025-01-02 18:23:08 -0600

    Web
    Actualización de las páginas e imágenes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,23 @@
 import streamlit as st
-
 # Ignore harmless warnings
 import warnings
 warnings.filterwarnings("ignore")
 
 
 #   -----------------------------------------------------------------------
-logo_path = "C:\\Users\\Celula1\\.streamlit\\img\\logo_small.png"
-icon = "C:\\Users\\Celula1\\.streamlit\\img\\icon.png"
+logo_path = "C:/Users/Celula1/app/static/logo_small.png"
+icon = "C:/Users/Celula1/app/static/icon.png"
+
 
 with st.sidebar:
     st.logo(image=logo_path, link='https://datlas.mx/', size='large', icon_image=logo_path)
 
 home = st.Page("EDA/home.py", title="Home", icon=":material/home:", default=True)
-EDA = st.Page("EDA/EDA.py", title="EDA", icon=":material/table:")
+EDA = st.Page("EDA/EDA.py", title="EDA", icon=":material/table:")  
 forecast = st.Page("Forecast/forecast.py", title="Pronóstico", icon=":material/search:")
 
-#pg = st.navigation({"Inicio": [home,EDA],"Tools": [forecast]})
-pg = st.navigation([home,EDA,forecast])
+pg = st.navigation([home,EDA,forecast], expanded=True)
 
 st.set_page_config(page_title='DEMO pronósticos', page_icon=icon)
 
 pg.run()
-
