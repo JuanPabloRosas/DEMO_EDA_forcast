a932bf7f28f0d1678b2ea8cb1738b6619ac75901
JuanPabloRosas <juanpablo.rosasbaldazo@gmail.com>
2025-02-27 12:57:52 -0600

    Add files via upload
    update file

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ warnings.filterwarnings("ignore")
 
 
 #   -----------------------------------------------------------------------
-#   LOCAL
-#icon = "C:/Users/Celula1/app/static/icon.png"
-#   PLOOMBER
+#   DOCKER
 icon = "static/icon.png"
 
 home = st.Page("EDA/home.py", title="Inicio", icon=":material/home:", default=True)
 EDA = st.Page("EDA/EDA.py", title="EDA", icon=":material/table:")  
 forecast = st.Page("Forecast/forecast.py", title="Pronóstico", icon=":material/search:")
-
 pg = st.navigation([home,EDA,forecast], expanded=True)
 
 st.set_page_config(page_title='EDA & Forecast', page_icon=icon)
